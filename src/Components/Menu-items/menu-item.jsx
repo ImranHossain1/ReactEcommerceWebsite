17a9f00093d6617fc754724dfa0712d79bf67e92
ImranHossain1 <imranhossai@gmail.com>
2020-10-30 20:27:19 +0100

    broken our homepage and fininshed the styling of our menu item pages

--- src/Components/Menu-items/menu-item.jsx
+++ src/Components/Menu-items/menu-item.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-
-const MenuItem = () => {
+import './menu-items.styles.scss';
+const MenuItem = ({title, imageUrl,size}) => {
     return (
-        <div>
-              <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>HATS</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
+        <div className={`${size} menu-item`}>
+            <div 
+                className="background-image"
+                style={{backgroundImage: `url(${imageUrl})`
+            }}/>
+            <div className='content'>
+            <h1 className='title'>{title}</h1>
+            <span className='subtitle'>SHOP NOW</span>
+            </div>
         </div>
     );
 };
--- src/Pages/Homepage/homepage.component.jsx
+++ src/Pages/Homepage/homepage.component.jsx
@@ -1,42 +1,13 @@
 import React from 'react';
+import Directory from '../../Components/directory/directory.component';
 import './homepage.styles.scss';
-const Homepage = () => {
-    return (
-        <div classname='homepage'>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>HATS</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>JACKETS</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>SNEAKERS</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>WOMENS</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>MENS</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
 
-export default Homepage;
+const HomePage = () => (
+  <div className='homepage'>
+    <h1>Welcome to my Homepage</h1>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
+
