029874bcd06a430b73f70ee9c003f8cd1b6a3552
ImranHossain1 <imranhossai@gmail.com>
2020-11-08 09:23:29 +0100

    SignIn has been updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
 import Homepage from './Pages/Homepage/homepage.component';
+import ShopPage from './Pages/shop/shop.component';
+import Header from './Components/Header/Header.component';
+import Sign from './Pages/Signin-and-singup-page/Sign';
 function App() {
   return (
-    <div className="App">
-      <Homepage />
+    <div>
+      <Header/>
+      <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={Sign}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/Pages/Signin-and-singup-page/Sign.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Sign.styles.scss';
+import SignIn from '../../Components/sign-in/sign-in.component';
+const Sign = () => (
+        <div className='sign-in-and-sign-up'>
+            <SignIn />
+        </div>
+    )
+
+
+export default Sign;
